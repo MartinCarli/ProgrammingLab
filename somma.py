4c7d7ab256266b9819a76e6e4cfe5a8c3ab3efc7
repl.it user <replituser@example.com>
2021-01-04 11:31:47 +0000

    Soluzione con returm'
    g
    
    '

--- somma.py
+++ somma.py
@@ -1,7 +1,6 @@
 def list_sum(the_list):
-    somma=0
+    sum=0
     for item in the_list:
-        somma= somma+item
-    print("La somma e` {}".format(somma))
-
-list_sum([1,4,5])
+        sum = sum + item
+    return sum
+print('Somma: {}'.format(list_sum([1,4,10])))
